f2c9290de396b1ff146c6b9bef1800b4df17127c
Sijia <97779113+dengsijia518@users.noreply.github.com>
2022-07-06 20:28:46 -0700

    First commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,9 @@
+package com.ecommerce.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PurchaseResponse {
+    private final String orderTrackingNumber;
+
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,69 @@
+package com.ecommerce.ecommerce.service;
+
+import com.ecommerce.ecommerce.dao.CustomerRepository;
+import com.ecommerce.ecommerce.dto.Purchase;
+import com.ecommerce.ecommerce.dto.PurchaseResponse;
+import com.ecommerce.ecommerce.entity.Customer;
+import com.ecommerce.ecommerce.entity.Order;
+import com.ecommerce.ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        //retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        //populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        //populate order with billing Address and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        //populate customer with order
+        Customer customer = purchase.getCustomer();
+
+        //check if this is an existing  customer
+        String theEmail = customer.getEmail();
+
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+
+        if (customerFromDB != null) {
+            //we found them ...
+            customer = customerFromDB;
+        }
+
+        customer.add(order);
+
+        //save ro the database
+        customerRepository.save(customer);
+
+        //return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        //generate a random UUID number (UUID version-4) Universially Unique IDentifier
+        //
+        return UUID.randomUUID().toString();
+
+    }
+}
